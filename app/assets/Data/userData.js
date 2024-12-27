cbbab59d73432d4b82f7f19da7142db7a796036f
hydrazin60 <sureshkhdk45@gmail.com>
2024-12-27 21:39:36 +0545

    user hard coaded data store

---
+++ app/assets/Data/userData.js
@@ -0,0 +1,32 @@
+export const Users = [
+  {
+    name: "Jiban",
+    id: "102",
+    gender: "Male",
+    image: "https://randomuser.me/api/portraits/men/1.jpg",
+  },
+  {
+    name: "Sita",
+    id: "103",
+    gender: "Female",
+    image: "https://randomuser.me/api/portraits/women/2.jpg",
+  },
+  {
+    name: "Ram",
+    id: "104",
+    gender: "Male",
+    image: "https://randomuser.me/api/portraits/men/3.jpg",
+  },
+  {
+    name: "Gita",
+    id: "105",
+    gender: "Female",
+    image: "https://randomuser.me/api/portraits/women/4.jpg",
+  },
+  {
+    name: "Hari",
+    id: "106",
+    gender: "Male",
+    image: "https://randomuser.me/api/portraits/men/5.jpg",
+  },
+];
--- app/components/home/Stories.jsx
+++ app/components/home/Stories.jsx
@@ -1,12 +1,20 @@
-import { View, Text } from "react-native";
+import { View, Text, Image, TouchableOpacity, ScrollView, StyleSheet } from "react-native";
 import React from "react";
-
+import { Users } from "../../assets/Data/userData";
 const Stories = () => {
   return (
     <View>
-      <Text style={{ color: "white" }}>Stories</Text>
+      <ScrollView horizontal showsHorizontalScrollIndicator={false}>
+        {Users.map((users, index) => (
+          <Image source={{ uri: users.image }} />
+        ))}
+      </ScrollView>
     </View>
   );
 };
 
 export default Stories;
+
+const style = StyleSheet.create({
+  
+})
